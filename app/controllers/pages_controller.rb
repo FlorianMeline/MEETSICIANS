2d4faea6f975ecabc96a88c9dc2d3fe5496689f2
pichereauvictor <pichereau.pro@hotmail.com>
2023-06-08 15:28:06 +0200

    landingpage/login

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    redirect_to new_user_session_path
     @user = current_user
   end
 
