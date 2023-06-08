815f1e34d76fa23ee8cbafee7b662d5352100ab1
Florian Meline <florian_meline@live.fr>
2023-06-08 15:56:45 +0200

    middle push chat

---
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -0,0 +1,44 @@
+import { Controller } from "@hotwired/stimulus"
+import { createConsumer } from "@rails/actioncable"
+
+export default class extends Controller {
+  static values = { chatroomId: Number, currentUserId: Number }
+  static targets = ["messages"]
+
+  connect() {
+    this.channel = createConsumer().subscriptions.create(
+      { channel: "ChatroomChannel", id: this.chatroomIdValue },
+      { received: data => this.#insertMessageAndScrollDown(data) }
+    )
+    console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
+  }
+
+  #insertMessageAndScrollDown(data) {
+    const currentUserIsSender = this.currentUserIdValue === data.sender_id
+    const messageElement = this.#buildMessageElement(currentUserIsSender, data.message)
+    this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+}
+
+  #buildMessageElement(currentUserIsSender, message) {
+    return `
+      <div class="message-row d-flex ${this.#justifyClass(currentUserIsSender)}">
+        <div class="${this.#userStyleClass(currentUserIsSender)}">
+          ${message}
+        </div>
+      </div>
+    `
+  }
+
+  #justifyClass(currentUserIsSender) {
+    return currentUserIsSender ? "justify-content-end" : "justify-content-start"
+  }
+
+  #userStyleClass(currentUserIsSender) {
+    return currentUserIsSender ? "sender-style" : "receiver-style"
+  }
+
+  resetForm(event) {
+    event.target.reset()
+  }
+}
