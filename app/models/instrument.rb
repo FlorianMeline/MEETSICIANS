86caa8413475130877f5e7c13db48483f32ae104
Oceane Villeneuve <oceanevilleneuve16@gmail.com>
2023-06-08 15:48:07 +0200

    index page

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,7 +1,8 @@
 class Instrument < ApplicationRecord
   INSTRUMENT_NAMES = %w[guitar drums piano vocal bass keyboard bagpipe other]
 
-  has_many :bands
+  has_many :bands, foreign_key: 'needed_instrument_id'
+
   has_many :users
   # Validation #
   validates :name, inclusion: INSTRUMENT_NAMES
