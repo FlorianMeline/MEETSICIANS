ed1125b22ed34ecd43c8145a2f0b79b787a1c3a0
Oceane Villeneuve <oceanevilleneuve16@gmail.com>
2023-06-09 13:06:46 +0200

    user profil

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,12 @@
 class ProfilesController < ApplicationController
   def show
     @user = current_user
+    @button_band = ""
+
+    if @user.band.present?
+      @button_band = "edit"
+    else
+      @button_band = "create"
+    end
   end
 end
