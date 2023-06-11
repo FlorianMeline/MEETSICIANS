ee62ec32bea44d2cb12a4c7c7be88bfc8b90b74d
Florian Meline <florian_meline@live.fr>
2023-06-11 17:31:29 +0200

    correction design chat

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -2,6 +2,8 @@ class ChatroomsController < ApplicationController
   def index
     @chatrooms_band = current_user.band_chatrooms
     @chatrooms_joined = current_user.chatrooms_joined
+    # raise
+    @preview = Message.last.content
   end
 
   def show
@@ -12,5 +14,4 @@ class ChatroomsController < ApplicationController
   def create
     @chatroom = Chatroom.new
   end
-
 end
