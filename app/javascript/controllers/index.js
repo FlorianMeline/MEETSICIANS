6b1ca37e50f566b5385524e7f97a076445d1f857
Florian Meline <florian_meline@live.fr>
2023-06-13 09:26:50 +0200

    correction dropdown ctrl

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,11 +7,14 @@ import { application } from "./application"
 import ChatroomSubscriptionController from "./chatroom_subscription_controller"
 application.register("chatroom-subscription", ChatroomSubscriptionController)
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
-
 import DropdownController from "./dropdown_controller"
 application.register("dropdown", DropdownController)
 
-import PreviewController from './preview_controller'
-application.register('preview', PreviewController)
+import EditProfilController from "./edit_profil_controller"
+application.register("edit-profil", EditProfilController)
+
+import HelloController from "./hello_controller"
+application.register("hello", HelloController)
+
+import PreviewController from "./preview_controller"
+application.register("preview", PreviewController)
