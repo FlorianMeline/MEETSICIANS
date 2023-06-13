8ccdccb3328ca9a2adb646ec12ea476022030892
Ulysse <ulysse@ik.me>
2023-06-13 12:32:18 +0200

    fonction add member to band OPERATIONNELLE

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,7 @@
 class UsersController < ApplicationController
-before_update :reload_page
-
-
   def update
     @user = User.find(params[:id])  # Assuming the user ID is passed as a parameter
-
     @user.update(band: nil)
-
-
     @band = Band.find(params[:id])
 
     if @user.update
