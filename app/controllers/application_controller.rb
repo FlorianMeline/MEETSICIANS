f7fd547c913c28c42821d41f04376e53481fa61a
Florian Meline <florian_meline@live.fr>
2023-06-13 11:19:31 +0200

    remise SSL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,4 @@ class ApplicationController < ActionController::Base
   #   bands_path
   # end
 
-  before_action :set_base_url
-
-  private
-
-  def set_base_url
-    request.base_url = 'http://www.meetsicians.fr' # Set the appropriate base URL
-  end
-
 end
